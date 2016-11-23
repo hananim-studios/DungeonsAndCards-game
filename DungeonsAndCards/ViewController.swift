47b58a3297f80327637b717a648d015e7deb3216
Matheus Martins <matheusmcrds@gmail.com>
2016-11-23 09:26:38 -0300

    added project files and gitignore

---
+++ DungeonsAndCards/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DungeonsAndCards
+//
+//  Created by Matheus Martins on 11/18/16.
+//  Copyright © 2016 hananim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
