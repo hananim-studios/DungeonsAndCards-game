0fd1a0e9da6d55a12ff23c5aaba9683a5c8a8420
Matheus Martins <matheusmcrds@gmail.com>
2016-12-01 16:21:11 -0300

    DungeonsAndCards/ManagePartyViewController.swift

---
+++ DungeonsAndCards/CGAffineTransform+Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  CGAffineTransform+Extensions.swift
+//  DungeonsAndCards
+//
+//  Created by Matheus Martins on 12/1/16.
+//  Copyright © 2016 hananim. All rights reserved.
+//
+
+import Foundation
---
+++ DungeonsAndCards/DIOCollectionViewDataSource.swift
@@ -0,0 +1,9 @@
+//
+//  DIOCollectionViewDataSource.swift
+//  DungeonsAndCards
+//
+//  Created by Matheus Martins on 12/1/16.
+//  Copyright © 2016 hananim. All rights reserved.
+//
+
+import Foundation
