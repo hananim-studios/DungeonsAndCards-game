ebbb15362bc841bbd2ed17c8f4a0647409d099b3
Matheus Martins <matheusmcrds@gmail.com>
2016-12-13 10:25:49 -0300

    finished battle autolayout

---
+++ DungeonsAndCards/FlowLayout.h
@@ -0,0 +1,13 @@
+//
+//  FlowLayout.h
+//  DungeonsAndCards
+//
+//  Created by Matheus Martins on 12/13/16.
+//  Copyright © 2016 hananim. All rights reserved.
+//
+
+#ifndef FlowLayout_h
+#define FlowLayout_h
+
+
+#endif /* FlowLayout_h */
---
+++ DungeonsAndCards/FlowLayout.m
@@ -0,0 +1,9 @@
+//
+//  FlowLayout.m
+//  DungeonsAndCards
+//
+//  Created by Matheus Martins on 12/13/16.
+//  Copyright © 2016 hananim. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
